62621e95c96ad8b85ae0260506216d32900bcca2
Strange-bs <80538114+Strange-bs@users.noreply.github.com>
2021-12-08 20:35:22 +0500

    testing

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,23 @@
 let questions = [{
   numb: 1,
-  question: "What does HTML stand for?",
-  answer: "Hyper Text Markup Language",
-  options: ["Hyper Text Preprocessor", "Hyper Text Markup Language", "Hyper Text Multiple Language", "Hyper Tool Multi Language"]
+  question: "A dictionary has data _____ words.",
+  answer: "about",
+  options: ["to", "about", "in", "at"]
 }, {
   numb: 2,
-  question: "What does CSS stand for?",
-  answer: "Cascading Style Sheet",
-  options: ["Common Style Sheet", "Colorful Style Sheet", "Computer Style Sheet", "Cascading Style Sheet"]
+  question: " _____ pass me the salt?",
+  answer: "Could",
+  options: ["Should", "Must", "Could", "Might"]
 }, {
   numb: 3,
-  question: "What does PHP stand for?",
-  answer: "Hypertext Preprocessor",
-  options: ["Hypertext Preprocessor", "Hypertext Programming", "Hypertext Preprogramming", "Hometext Preprocessor"]
+  question: "Did you _____ who I meant?",
+  answer: "know",
+  options: ["known", "knew", "know", "got"]
 }, {
   numb: 4,
-  question: "What does SQL stand for?",
-  answer: "Structured Query Language",
-  options: ["Stylish Question Language", "Stylesheet Query Language", "Statement Question Language", "Structured Query Language"]
+  question: "He _____ English much _____ he writes it.",
+  answer: "speaks / better than",
+  options: ["speaks / better than", "speak / worse", "speaks / clearer than", "speak /  a lot"]
 }, {
   numb: 5,
   question: "What does XML stand for?",
